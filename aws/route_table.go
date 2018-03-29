bc40ba096ddede63f0cf1369d9347579d0e2c6a9
kizkoh <mac-daisuki@live.jp>
2018-03-29 12:22:18 +0900

    Fix output when retry API

--- aws/route_table.go
+++ aws/route_table.go
@@ -72,7 +72,7 @@ func (t *RouteTable) ReplaceRoute(vip, instance string) error {
 	var changed string
 	for i := 0; i <= retry; i++ {
 		if i > 0 {
-			fmt.Printf("Retry (%v/%v): confirm route table destination changes", i, retry)
+			fmt.Printf("Retry (%v/%v): confirm route table destination changes\n", i, retry)
 		}
 		changed, err = t.e.getInstanceIdByDest(ctx, retry, retryWait, routeTableId, destinationCidrBlock)
 		if err != nil {
